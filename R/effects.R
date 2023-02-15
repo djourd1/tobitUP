0c41c51cf3f8130fc164b149d3cf85f62700c5b3
Damien Jourdain <damien.jourdain@cirad.fr>
2023-02-15 09:47:56 +0200

    Added se.effect.uncond

--- R/effects.R
+++ R/effects.R
@@ -164,3 +164,157 @@ summary.tobitEffect <- function(x,
                signif.stars = signif.stars)
 }
 
+effect.uncond.factor = function(X, b, s,  varname){
+  # Reminder:
+  # E(y|X) =  Phi(xbs) x xb + s * phi(xbs)
+
+  X0 = X
+  X0[, varname] <- 0
+  xb0 = X0 %*% b
+  xbs0 = xb0/s
+
+  X1 = X
+  X1[, varname] <- 1
+  xb1 = X1 %*% b
+  xbs1 = xb1/s
+
+  E1 = pnorm(xbs1) * xb1 + s * dnorm(xbs1)
+  E0 = pnorm(xbs0) * xb0 + s * dnorm(xbs0)
+
+  res = mean(E1-E0)
+  return(res)
+}
+
+
+
+
+effect.uncond <- function(model){
+  # Check that model is an AER object
+  if (! inherits(model, "survreg")) {
+    stop("Error: object is not an AER object")
+  }
+
+  # Extract information
+  x = model.matrix(model)
+  b = model$coefficients
+  s = model$scale
+  cname <- names(b)
+
+  # Calculate the XB and XB/S
+  xb = x%*% b
+  xbs = xb/s
+
+  # Calculate the mean scaling factor to obtain
+  # the marginal effects on unconditionals for
+  # continuous variables
+  scale.uncond = mean(pnorm(xbs))
+
+  # Separate numerics from factors
+  nnames = varClass(model)$nnames
+  fnames = varClass(model)$fnames
+
+  # Reporting of marginal effects on conditional
+  # for continuous variables
+  if (is.null(nnames)){
+    eff1 <- NULL
+    }
+  else{
+    eff1 <- b[nnames]  * scale.uncond
+  }
+
+  # Reporting of marginal effects on conditional
+  # for factors
+  eff2 <- NULL
+  if (!is.null(fnames)){
+    eff2 <- b[fnames] # use b to initiate a named vector
+    for (i in fnames){
+      eff2[i] <- effect.uncond.factor(x,b,s, i)
+    }
+  }
+  ## Combine tables
+  res = NULL
+  if (is.null(fnames) & is.null(nnames)){
+    table <- NULL
+  } else {
+    res <- c(eff1, eff2)
+  }
+
+  return(list(vec= res, vecN = eff1, vecF=eff2))
+}
+
+
+
+#' Calculates the standard errors of the
+#' partial effects on unconditional expected values
+#' It requires a tobit model estimated with the  AER package
+#'
+#' @param model
+#'
+#' + model: The AER model to use
+#' + R: the number of repetitions
+#' + data: the data frame that was used for the model
+#'
+#' @return
+#' - call: the call of the model
+#' - table: A 4 x k table gathering the mean of
+#' the average partial effets
+#' - R: the number of repetitions
+#'
+#' @examples
+#' library(wooldridge)
+#' data(mroz)
+#' model <- tobit(hours ~ nwifeinc + educ, data=mroz)
+#' sem <- se.effect.uncond(model)
+#' sem  #raw output
+#' summary.uncond.eff(sem)  #output the formatted table
+#'
+#' @export
+se.effect.uncond <- function(model, R, data){
+  effs <- NULL
+  form = model$formula
+  b = model$coefficients
+  nr = nrow(dataf)
+  for (i in 1:R){
+    new_sample = sample(1:nr, size = nr, replace = TRUE)
+    df = data.frame(dataf[new_sample, ])
+    new_model <- survreg(form, dist= "gaussian", data=df)
+    effs = rbind(effs, effect.uncond(new_model)$vec  )
+  }
+
+  table <- matrix(rep(b[-1], 4), ncol = 4)
+  dimnames(table) <- list(names(b[-1]),
+                          c("Value", "Std. Error", "z", "p")  )
+  table[, 1] <- apply(effs, 2, mean)
+  table[, 2] <- apply(effs, 2, sd)
+  table[, 3] <- table[,1] / table[,2]
+  table[, 4] <- 2*pnorm(-abs(table[,3]))
+
+  # returns object
+  res <- model[match(c('call', 'coefficients'),
+                     names(model), nomatch=0)]
+  res <- c(res, list(table=table, R = R))
+  return(res)
+}
+
+#'
+#' @export
+summary.uncond.eff <- function(x,
+                                digits = max(options()$digits - 4, 3),
+                                signif.stars=TRUE, ...) {
+
+  # if(is.null(digits))
+  #     digits <- options()$digits
+  # cat("\nCall:\n")
+  # dput(x$call)
+  cat("\nAverage Partial Effects for Unconditionals:\n")
+  cat("(The standard errors calculating using bootstrap technique)\n\n")
+  #    printCoefmat(x$table, digits = digits, signif.stars=signif.stars,
+  #                 P.values=TRUE, has.Pvalue=TRUE)
+  printCoefmat(x$table, digits = digits,
+               has.Pvalue = TRUE,
+               P.values = TRUE,
+               signif.stars = signif.stars)
+  cat("\nNo of repetitions: ")
+  dput(x$R)
+
+}
